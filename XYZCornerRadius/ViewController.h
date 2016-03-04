3b6172e0ecee264d92d8d09fae85b3bc08b9d25d
xyzlee <xyzlee@xyz.com>
2016-03-04 10:38:10 +0800

    Initial Commit

---
+++ XYZCornerRadius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYZCornerRadius
+//
+//  Created by iMac on 16/3/4.
+//  Copyright (c) 2016年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XYZCornerRadius/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XYZCornerRadius
+//
+//  Created by iMac on 16/3/4.
+//  Copyright (c) 2016年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
